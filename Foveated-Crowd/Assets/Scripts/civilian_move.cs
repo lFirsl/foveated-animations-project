548a3d7c03e834911a539df48c01fec85634e511
Florin-Vladimir Stancu <sc20fvs@leeds.ac.uk>
2024-03-26 23:01:38 +0000

    Add limit to view limitation to Civilian Move script.

--- Foveated-Crowd/Assets/Scripts/civilian_move.cs
+++ Foveated-Crowd/Assets/Scripts/civilian_move.cs
@@ -8,6 +8,7 @@ public class CivilianMove : MonoBehaviour
     //Private components. Fetched during Start.
     private Animator _anim;
     private NavMeshAgent _agent;
+    private Camera _mainCamera;
     
     //Serialized Private Field
     public float wanderRadius;
@@ -16,6 +17,7 @@ public class CivilianMove : MonoBehaviour
     public float maxSpeed;
     public float slowFastThreshold;
     public int stopThreshold;
+    public bool limitToView = false;
     
     [SerializeField] private float MinimumAnimationSpeed = 0.8f;
     [SerializeField] private float MaximumAnimationSpeed = 1.2f;
@@ -29,6 +31,7 @@ public class CivilianMove : MonoBehaviour
     {
         _anim = GetComponent<Animator>();
         _agent = GetComponent<NavMeshAgent>();
+        _mainCamera = Camera.main;
         _agent.SetDestination(RandomNavSphere(transform.position, wanderRadius, -1));
         
         _runningID = Animator.StringToHash(RunningString);
@@ -76,15 +79,39 @@ public class CivilianMove : MonoBehaviour
     }
 
     
-    private static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask) {
-        Vector3 randDirection = Random.insideUnitSphere * dist;
- 
-        randDirection += origin;
- 
+    private Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
+    {
+        Vector3 randDirection;
         NavMeshHit navHit;
- 
+        
+        //If necessary, ensure that our randomized nav position is within the view.
+        //Especially useful for the main focus point - it gets problematic if it leaves the viewport.
+        if (limitToView)
+        {
+            int max = 0;
+            while (max < 30)
+            {
+                Vector3 temp = new Vector3();
+                temp.x = Random.Range(0.1f, 0.9f);
+                temp.y = Random.Range(0.1f, 0.9f);
+                Ray ray = _mainCamera.ViewportPointToRay(temp);
+                RaycastHit hit;
+                
+                if (Physics.Raycast(ray, out hit))
+                {
+                    NavMesh.SamplePosition (hit.point, out navHit, dist, layermask);
+                    return navHit.position;
+                }
+                //Failed - increment max counter.
+                max++;
+            }
+        }
+        
+        //IF limitToView is off - or the algorithm fails - use this instead.
+        randDirection = Random.insideUnitSphere * dist + origin;
+
         NavMesh.SamplePosition (randDirection, out navHit, dist, layermask);
- 
+
         return navHit.position;
     }
 }
