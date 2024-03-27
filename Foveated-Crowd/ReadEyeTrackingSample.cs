f2369552bc503f1e66bae7b11566e3748857ec46
Florin-Vladimir Stancu <sc20fvs@leeds.ac.uk>
2024-03-27 13:37:34 +0000

    Add ViveSR SDK

---
+++ Foveated-Crowd/ReadEyeTrackingSample.cs
@@ -0,0 +1,67 @@
+
+using System.Runtime.InteropServices;
+using UnityEngine;
+using ViveSR.anipal.Eye;
+
+public class ReadEyeTrackingSample : MonoBehaviour
+{
+    public struct RayInfo
+    {
+        public Vector3 pos;
+        public Vector3 dir;
+    };
+
+    private static EyeData_v2 eyeData = new EyeData_v2();
+    private bool eye_callback_registered = false;
+    private RayInfo rightRay;
+    private RayInfo leftRay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!SRanipal_Eye_Framework.Instance.EnableEye)
+        {
+            enabled = false;
+            return;
+        }
+    }
+
+    public RayInfo getRightRay() { return rightRay; }
+    public RayInfo getleftRay() { return leftRay; }
+    // Update is called once per frame
+    void Update()
+    {
+        if (SRanipal_Eye_Framework.Status != SRanipal_Eye_Framework.FrameworkStatus.WORKING &&
+                        SRanipal_Eye_Framework.Status != SRanipal_Eye_Framework.FrameworkStatus.NOT_SUPPORT) return;
+
+        if (SRanipal_Eye_Framework.Instance.EnableEyeDataCallback == true && eye_callback_registered == false)
+        {
+            SRanipal_Eye_v2.WrapperRegisterEyeDataCallback(Marshal.GetFunctionPointerForDelegate((SRanipal_Eye_v2.CallbackBasic)EyeCallback));
+            eye_callback_registered = true;
+        }
+        else if (SRanipal_Eye_Framework.Instance.EnableEyeDataCallback == false && eye_callback_registered == true)
+        {
+            SRanipal_Eye_v2.WrapperUnRegisterEyeDataCallback(Marshal.GetFunctionPointerForDelegate((SRanipal_Eye_v2.CallbackBasic)EyeCallback));
+            eye_callback_registered = false;
+        }
+
+        if (eye_callback_registered) 
+        {
+            SRanipal_Eye_v2.GetGazeRay(GazeIndex.LEFT, out leftRay.pos, out leftRay.dir, eyeData);
+            SRanipal_Eye_v2.GetGazeRay(GazeIndex.RIGHT, out rightRay.pos, out rightRay.dir, eyeData);
+        }
+
+    }
+    private void Release()
+    {
+        if (eye_callback_registered == true)
+        {
+            SRanipal_Eye_v2.WrapperUnRegisterEyeDataCallback(Marshal.GetFunctionPointerForDelegate((SRanipal_Eye_v2.CallbackBasic)EyeCallback));
+            eye_callback_registered = false;
+        }
+    }
+
+    private static void EyeCallback(ref EyeData_v2 eye_data)
+    {
+        eyeData = eye_data;
+    }
+}
