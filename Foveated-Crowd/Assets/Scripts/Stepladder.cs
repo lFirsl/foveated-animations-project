b2738d44873379e7d7a8f2278ec562cdc0919c0d
Florin-Vladimir Stancu <sc20fvs@leeds.ac.uk>
2024-11-03 14:36:24 +0000

    Add polish changes to scenes and animator controller.

--- Foveated-Crowd/Assets/Scripts/Stepladder.cs
+++ Foveated-Crowd/Assets/Scripts/Stepladder.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,6 +10,7 @@ public class Stepladder : MonoBehaviour
     [SerializeField] private float tempFactor = 0.1f;
     [SerializeField] private float constatFoveaArea = 0.1f;
     [SerializeField] private float factorIncrementStep = 0.25f;
+    [SerializeField] private int noFoveationTime = 20;
     // Start is called before the first frame update
     
     private IEnumerator stepladderProcedure()
@@ -16,10 +18,10 @@ public class Stepladder : MonoBehaviour
         yield return new WaitForSeconds(1);
         FocusPointSphere focus = FindObjectOfType<FocusPointSphere>();
         focus.stopThreshold = 3f;
-        focus.foveaArea = 2f;
-        focus.foveationFactor = tempFactor;
+        focus.foveaArea = 3f;
+        focus.foveationFactor = 0.0001f;
         
-        yield return new WaitForSeconds(9);
+        yield return new WaitForSeconds(Math.Max(1, noFoveationTime-1));
         focus.foveaArea = constatFoveaArea;
         focus.foveationFactor = tempFactor;
         while(focus.foveationFactor < factorTestThreshold)
